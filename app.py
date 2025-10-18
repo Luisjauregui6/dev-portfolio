62616af3273c3eb5f6eb1e8c859812e53fc240a4
luis <luisjauregui221@gmail.com>
2025-10-18 13:19:48 -0600

    updates and fixes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from utils.github_api import get_github_data
 
 app = Flask(__name__)
@@ -7,11 +7,26 @@ app = Flask(__name__)
 def portfolio():
     data = None
     hint = "Try: Luisjauregui6"  
+
     if request.method == 'POST':
         username = request.form.get('username')
         if username:
             data = get_github_data(username)
+            if username.lower() == 'luisjauregui6':
+                data['is_owner'] = True
+            else:
+                data['is_owner'] = False   
+
     return render_template("portfolio.html", data=data, hint=hint)
+
+# contact form 
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    print(f'new message from {name} ({email}): {message}')
+    return redirect(url_for('porfolio'))
 
 if __name__ == "__main__":
     app.run(debug=True)
